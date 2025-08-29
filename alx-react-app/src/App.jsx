e280067916a27eb5a5d9d310b7424d62ea724d8e
JAMES AMBALA <ambalajames729@gmail.com>
2025-08-29 10:27:07 +0300

    added user profile and updated welome message.js

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
       <Footer />
 
       <h1>Vite + React</h1>
@@ -20,13 +27,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
