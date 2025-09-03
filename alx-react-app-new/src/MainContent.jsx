adccda62c478152abea4b43572449ba1355e4a1c
JAMES AMBALA <ambalajames729@gmail.com>
2025-09-03 22:10:53 +0300

    created new alx-fe-react-app-new

---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,16 @@
+ function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#eef', minHeight: '60vh' }}>
+      <h2 style={{ textAlign: 'center' }}>Welcome to My Favorite Cities Page</h2>
+      <p style={{ textAlign: 'center' }}>Explore some cool cities and learn interesting facts about them!</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
+/*<div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+   <h2 style={{ color: 'blue' }}>{props.name}</h2>
+   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+   <p>Bio: {props.bio}</p>
+ </div>*/
